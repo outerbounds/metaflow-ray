5805b2e8813d21b22b00f7c1ce66a3a7b8a39300
Eddie Mattia <eddie@outerbounds.com>
2023-10-31 15:32:25 -0700

    remove deps

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_namespace_packages
 
-version = "0.0.3"
+version = "0.0.4"
 
 setup(
     name="metaflow-ray",
@@ -12,5 +12,5 @@ setup(
     py_modules=[
         "metaflow_extensions",
     ],
-    install_requires=["metaflow"],
+    install_requires=[],
 )
