4034991c12bdbfeb2232a10443b339a9dc4e2727
Madhur Tandon <madhur@outerbounds.co>
2024-12-24 16:59:41 +0530

    fix example

--- examples/ray_torch_lightning/flow.py
+++ examples/ray_torch_lightning/flow.py
@@ -1,5 +1,38 @@
+from typing import Any, Dict
+from functools import partial
 from gpu_profile import gpu_profile
-from metaflow import FlowSpec, Parameter, step, pypi, kubernetes, metaflow_ray
+from metaflow import FlowSpec, Parameter, step, pypi, kubernetes, card, current, metaflow_ray
+
+
+def train_func(config: Dict[str, Any], batch_size: int, num_epochs: int):
+    import os
+    import lightning.pytorch as pl
+    from model import MNISTClassifier
+    from dataloader import MNISTDataModule
+    from ray.train.lightning import (
+        prepare_trainer,
+        RayLightningEnvironment,
+        RayTrainReportCallback,
+        RayDDPStrategy
+    )
+
+    model = MNISTClassifier(config=config)
+    dm = MNISTDataModule(batch_size=batch_size)
+    logger = pl.loggers.TensorBoardLogger(
+        save_dir=os.getcwd(), name="tune-ptl-example", version="."
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=num_epochs,
+        accelerator="gpu",
+        strategy=RayDDPStrategy(),
+        plugins=[RayLightningEnvironment()],
+        callbacks=[RayTrainReportCallback()],
+        logger=logger,
+    )
+
+    trainer = prepare_trainer(trainer)
+    trainer.fit(model, datamodule=dm)
 
 
 class RayLightningTuneFlow(FlowSpec):
@@ -19,104 +52,61 @@ class RayLightningTuneFlow(FlowSpec):
         help="Batch size"
     )
 
-    def tune_mnist_asha(self, trainer, config, scheduler, number_samples=10):
-        from ray import air, tune
+    def _do_ray_job(self):
+        import ray
+        from ray import tune, air
+        from ray.train.torch import TorchTrainer
+        from ray.tune.schedulers import ASHAScheduler
+
+        ray.init()
+
+        config = {
+            "layer_1_size": tune.choice([32, 64, 128]),
+            "layer_2_size": tune.choice([64, 128, 256]),
+            "lr": tune.loguniform(1e-4, 1e-1),
+        }
+
+        scheduler = ASHAScheduler(
+            max_t=self.num_epochs,
+            grace_period=1,
+            reduction_factor=2
+        )
+
+        trainable = partial(
+            train_func,
+            batch_size=self.batch_size,
+            num_epochs=self.num_epochs,
+        )
+
+        ray_trainer = TorchTrainer(
+            trainable,
+            scaling_config=air.ScalingConfig(
+                num_workers=4, use_gpu=True, resources_per_worker={"CPU": 8, "GPU": 1},
+            ),
+        )
 
         tuner = tune.Tuner(
-            trainer,
-            param_space={"lightning_config": config},
+            ray_trainer,
+            param_space={"train_loop_config": config},
             tune_config=tune.TuneConfig(
-                metric="ptl/val_accuracy",
+                metric="val/accuracy",
                 mode="max",
-                num_samples=number_samples,
+                num_samples=self.num_samples,
                 scheduler=scheduler,
             ),
             run_config=air.RunConfig(
                 name="tune_mnist_asha",
+                storage_path=current.ray_storage_path,
+                checkpoint_config=air.CheckpointConfig(
+                    num_to_keep=2,
+                    checkpoint_score_attribute="val/accuracy",
+                    checkpoint_score_order="max",
+                )
             ),
         )
 
         results = tuner.fit()
-        best_result = results.get_best_result(metric="ptl/val_accuracy", mode="max")
-        return best_result
-
-    def _do_ray_job(self):
-        import os
-        import ray
-        from ray import tune
-        from ray.tune.schedulers import ASHAScheduler
-        from ray.air.config import RunConfig, ScalingConfig, CheckpointConfig
-        from ray.train.lightning import LightningTrainer, LightningConfigBuilder
-
-        from model import MNISTClassifier
-        from dataloader import MNISTDataModule
-
-        from pytorch_lightning.loggers import TensorBoardLogger
-
-        ray.init()
-
-        dm = MNISTDataModule(batch_size=self.batch_size)
-        logger = TensorBoardLogger(
-            save_dir=os.getcwd(), name="tune-ptl-example", version="."
-        )
-
-         # Static configs that does not change across trials
-        static_lightning_config = (
-            LightningConfigBuilder()
-            .module(cls=MNISTClassifier)
-            .trainer(max_epochs=self.num_epochs, accelerator="gpu", logger=logger)
-            .fit_params(datamodule=dm)
-            .checkpointing(monitor="ptl/val_accuracy", save_top_k=2, mode="max")
-            .build()
-        )
-
-        # Searchable configs across different trials
-        searchable_lightning_config = (
-            LightningConfigBuilder()
-            .module(
-                config={
-                    "layer_1_size": tune.choice([32, 64, 128]),
-                    "layer_2_size": tune.choice([64, 128, 256]),
-                    "lr": tune.loguniform(1e-4, 1e-1),
-                }
-            )
-            .build()
-        )
-
-        # Make sure to also define an AIR CheckpointConfig here
-        # to properly save checkpoints in AIR format.
-        run_config = RunConfig(
-            checkpoint_config=CheckpointConfig(
-                num_to_keep=2,
-                checkpoint_score_attribute="ptl/val_accuracy",
-                checkpoint_score_order="max",
-            ),
-        )
-
-        scheduler = ASHAScheduler(
-            max_t=self.num_epochs, grace_period=1, reduction_factor=2
-        )
-
-        scaling_config = ScalingConfig(
-            num_workers=4,
-            use_gpu=True,
-            resources_per_worker={"CPU": 7, "GPU": 1},
-        )
-
-        lightning_trainer = LightningTrainer(
-            lightning_config=static_lightning_config,
-            scaling_config=scaling_config,
-            run_config=run_config,
-        )
-
-        analysis = self.tune_mnist_asha(
-            trainer=lightning_trainer,
-            config=searchable_lightning_config,
-            scheduler=scheduler,
-            number_samples=self.num_samples
-        )
-
-        print(analysis)
+        self.best_result = results.get_best_result(metric="val/accuracy", mode="max")
 
     @step
     def start(self):
@@ -131,12 +121,10 @@ class RayLightningTuneFlow(FlowSpec):
         {
             "torch": "2.5.1",
             "torchvision": "0.20.1",
-            "ray[tune,train]": "2.40.0",
-            "pandas": "2.2.3",
+            "ray[air]": "2.40.0",
             "matplotlib": "3.10.0",
-            "pytorch-lightning": "2.4.0",
+            "lightning": "2.4.0",
             "torchmetrics": "1.6.0",
-            "tensorboard": "2.18.0",
         }
     )
     @step
@@ -146,11 +134,14 @@ class RayLightningTuneFlow(FlowSpec):
 
     @step
     def join(self, inputs):
+        self.merge_artifacts(inputs)
         self.next(self.end)
 
+    @card
+    @pypi(packages={"ray[train]": "2.40.0"})
     @step
     def end(self):
-        pass
+        self.metrics_df = self.best_result.metrics_dataframe
 
 
 if __name__ == "__main__":
